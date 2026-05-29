c02bc1d0e3db38b1cd3c0bfa621166d7a071b0b3
Yu <yu@matrix.works>
2026-05-29 12:26:15 +0800

    fix: replace green with blue grey in default color of theme

--- lib/src/settings/theme/provider.dart
+++ lib/src/settings/theme/provider.dart
@@ -10,7 +10,7 @@ part 'provider.g.dart';
 
 /// The default color provider
 @Riverpod(keepAlive: true)
-Color defaultColorSeed(DefaultColorSeedRef ref) => Colors.green;
+Color defaultColorSeed(DefaultColorSeedRef ref) => Colors.blueGrey;
 
 /// Provides the settings
 @Riverpod(keepAlive: true)
